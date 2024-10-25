67eec0343aa6059b36e72418447fbe0560859ec8
Jaron <jaron.jones@gmail.com>
2024-10-25 17:20:47 -0600

    added sample table data

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	http.HandleFunc("/", dashboardHandler)
 	http.HandleFunc("/data", dataHandler)
+	http.HandleFunc("/table", tableHandler)
 	http.Handle("/static", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
 	http.ListenAndServe(":8080", nil)
 }
@@ -25,4 +26,21 @@ func dataHandler(w http.ResponseWriter, r *http.Request) {
 		Message: "Jason sucks htmx!",
 	}
 	tmpl.Execute(w, data)
+}
+
+func tableHandler(w http.ResponseWriter, r *http.Request) {
+	tmple := template.Must(template.ParseFiles("templates/table.html"))
+	data := []struct {
+		ID    int
+		Name  string
+		Value float64
+	}{
+		{1, "Item One", 123.45},
+		{2, "Item Two", 678.98},
+		{3, "Item Three", 456.71},
+		{4, "Item Four", 456.73},
+		{5, "Item Five", 56.44},
+		{6, "Item Six", 456.70},
+	}
+	tmple.Execute(w, data)
 }
