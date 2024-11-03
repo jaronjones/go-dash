852a10792951d2b5a1431807ec5325a575d73396
Jaron <jaron.jones@gmail.com>
2024-11-02 22:49:13 -0600

    adding sse capability

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go-dash
 
 go 1.23.2
+
+require (
+	maragu.dev/gomponents v1.0.0 // indirect
+	maragu.dev/gomponents-htmx v0.6.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", dashboardHandler)
 	http.HandleFunc("/data", dataHandler)
 	http.HandleFunc("/table", tableHandler)
+	http.HandleFunc("/events", sseHandler)
 	http.Handle("/static", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
 	http.ListenAndServe(":8080", nil)
 }
 
 func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("dashboardHandler was called")
 	tmpl := template.Must(template.ParseFiles("templates/dashboard.html"))
-	tmpl.Execute(w, nil)
+	fmt.Println("this was called 2nd")
+	err := tmpl.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error executing template:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("this was called 3rd")
 }
 
 func dataHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("dataHandler was called")
 	tmpl := template.Must(template.ParseFiles("templates/data.html"))
 	data := struct {
 		Message string
@@ -29,13 +41,14 @@ func dataHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func tableHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("tableHandler was called")
 	tmple := template.Must(template.ParseFiles("templates/table.html"))
 	data := []struct {
 		ID    int
 		Name  string
 		Value float64
 	}{
-		{1, "Item One", 123.45},
+		{1, "Test One", 123.45},
 		{2, "Item Two", 678.98},
 		{3, "Item Three", 456.71},
 		{4, "Item Four", 456.73},
@@ -43,4 +56,23 @@ func tableHandler(w http.ResponseWriter, r *http.Request) {
 		{6, "Item Six", 456.70},
 	}
 	tmple.Execute(w, data)
+}
+
+func sseHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("sseHandler was called")
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	for i := range 5 {
+		fmt.Fprintf(w, "data: Message %d\n\n", i)
+		flusher.Flush()
+		time.Sleep(2 * time.Second)
+	}
 }
