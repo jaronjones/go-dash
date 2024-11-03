3d6fd89d694b808b2cbb5a70e7686915684cbcee
Jaron <jaron.jones@gmail.com>
2024-11-02 23:58:16 -0600

    using logrus
    implented logrus logger, formatting using json

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module go-dash
 go 1.23.2
 
 require (
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 	maragu.dev/gomponents v1.0.0 // indirect
 	maragu.dev/gomponents-htmx v0.6.1 // indirect
 )
--- main.go
+++ main.go
@@ -4,45 +4,69 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	log.Info("Starting the server")
 	http.HandleFunc("/", dashboardHandler)
 	http.HandleFunc("/data", dataHandler)
 	http.HandleFunc("/table", tableHandler)
 	http.HandleFunc("/events", sseHandler)
 	http.Handle("/static", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
-	http.ListenAndServe(":8080", nil)
+
+	listener := http.ListenAndServe(":8080", nil)
+
+	if listener != nil {
+		log.Fatal("Failed to start the server")
+	}
+}
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+
+	log.SetOutput(os.Stdout)
+
+	log.SetLevel(log.InfoLevel)
 }
 
 func dashboardHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("dashboardHandler was called")
+	log.Info("dashboardHandler was called")
+
 	tmpl := template.Must(template.ParseFiles("templates/dashboard.html"))
-	fmt.Println("this was called 2nd")
+
 	err := tmpl.Execute(w, nil)
 	if err != nil {
 		fmt.Println("Error executing template:", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	fmt.Println("this was called 3rd")
 }
 
 func dataHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("dataHandler was called")
+	log.Info("DataHandler was called")
 	tmpl := template.Must(template.ParseFiles("templates/data.html"))
 	data := struct {
 		Message string
 	}{
 		Message: "Jason sucks htmx!",
 	}
-	tmpl.Execute(w, data)
+
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		log.Error("Error executing data template")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func tableHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("tableHandler was called")
-	tmple := template.Must(template.ParseFiles("templates/table.html"))
+	log.Info("TableHandler was called")
+
+	tmpl := template.Must(template.ParseFiles("templates/table.html"))
+
 	data := []struct {
 		ID    int
 		Name  string
@@ -55,13 +79,19 @@ func tableHandler(w http.ResponseWriter, r *http.Request) {
 		{5, "Item Five", 56.44},
 		{6, "Item Six", 456.70},
 	}
-	tmple.Execute(w, data)
+
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		log.Error("TableHandler execution error")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func sseHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("sseHandler was called")
+	log.Info("Executing event handler")
 	flusher, ok := w.(http.Flusher)
 	if !ok {
+		log.Fatal("Client unable to support streaming!")
 		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
 		return
 	}
